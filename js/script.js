99c2c0133e94680fdf0cba69d583de83369b27e4
HARILAL AV <harilal.talrop@gmail.com>
2021-08-17 16:46:44 +0530

    updated ui

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
-    
-});
+
+const LINKS = document.querySelectorAll("a");
+for (var i = 0 ; i < LINKS.length; i++) {
+    console.log(i);
+    LINKS[i].addEventListener('click',function(event){
+        event.preventDefault();
+    });
+ }
